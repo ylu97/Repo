9271adf173b1398e9d1afae892567a85941f131d
ylu97 <ylu97@dons.usfca.edu>
2025-02-13 17:07:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -16,25 +16,20 @@ client = Groq(
 
 client = instructor.from_groq(client, mode=instructor.Mode.TOOLS)
 
-resp = client.chat.completions.create(
-    model="mixtral-8x7b-32768",
-    messages=[
-        {
-            "role": "user",
-            "content": "Tell me about the company Google",
-        }
-    ],
-    response_model=Character,
-)
-print(resp.model_dump_json(indent=2))
-"""
-{
-  "name": "Tesla",
-  "fact": [
-    "electric vehicle manufacturer",
-    "solar panel producer",
-    "based in Palo Alto, California",
-    "founded in 2003 by Elon Musk"
-  ]
-}
-"""
+while True:
+    user_input = input("Please enter a subject that you want to search (or type 'Quit' to exit): ")
+    if user_input.strip().lower() == "quit":
+        print("Exiting now...")
+        break
+    
+    resp = client.chat.completions.create(
+        model="mixtral-8x7b-32768",
+        messages=[
+            {
+                "role": "user",
+                "content": user_input,
+            }
+        ],
+        response_model=Character,
+    )
+    print(resp.model_dump_json(indent=2))
